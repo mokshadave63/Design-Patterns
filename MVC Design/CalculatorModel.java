3e263bbabcbff60f7cbcf279217a5a11bac89328
Moksha Dave <115356914+mokshadave63@users.noreply.github.com>
2024-07-06 17:22:35 +0530

    MVC Design components

---
+++ MVC Design/CalculatorModel.java
@@ -0,0 +1,16 @@
+package MVC;
+
+public class CalculatorModel {
+	
+	private int calcValue;
+	
+	public void addTwoNumbers(int fn,int sn)
+	{
+	calcValue=fn+sn;
+	}
+
+	public int getCalculationValue()
+	{
+	return calcValue;
+	}
+}
